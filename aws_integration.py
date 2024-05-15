0b96f6d342e4ceb6a69d4e86dec63d6bddfba14f
Full Name <email@amazon.com>
2024-05-15 16:20:00 +0000

    add iam roles support

--- aws_integration.py
+++ aws_integration.py
@@ -8,11 +8,12 @@ AWS_SECRET_ACCES_KEY_LEN = 40
 
 
 class AWSClientSettings(BaseModel):
-    aws_access_key_id: str = Field(default="")
-    aws_secret_access_key: str = Field(default="")
-    region_name: str = Field(default="us-east-1")
-    credentials_profile_name: str = Field(default="")
-    endpoint_url: str = Field(default="")
+    aws_access_key_id: str = Field(default="", description="AWS access key ID for authentication.")
+    aws_secret_access_key: str = Field(default="", description="AWS secret access key for authentication.")
+    region_name: str = Field(default="us-east-1", description="Default AWS region for the client.")
+    credentials_profile_name: str = Field(default="", description="Name of the AWS credentials profile to use.")
+    endpoint_url: str = Field(default="", description="Custom endpoint URL, if using a non-standard AWS service endpoint.")
+    iam_role_assigned: bool = Field(default=False, description="Indicates if the required IAM role is assigned.")
 
     @model_validator(mode="after")
     def validate(cls, v):
@@ -20,17 +21,18 @@ class AWSClientSettings(BaseModel):
         available_regions = session.get_available_regions("ec2")
         if v.region_name not in available_regions:
             raise ValueError(f"{v.region_name} is not a valid AWS region")
-
-        if not v.credentials_profile_name:
-            if not v.aws_access_key_id or not v.aws_secret_access_key:
-                raise ValueError(
-                    "Either provide a credentials profile name or both aws_access_key_id and aws_secret_access_key"
-                )
-            elif not (
-                len(v.aws_access_key_id) == AWS_ACCES_KEY_LEN
-                and len(v.aws_secret_access_key) == AWS_SECRET_ACCES_KEY_LEN
-            ):
-                raise ValueError("The access key or secret access key is invalid!")
+        
+        if not v.iam_role_assigned:
+            if not v.credentials_profile_name:
+                if not v.aws_access_key_id or not v.aws_secret_access_key:
+                    raise ValueError(
+                        "Either provide a credentials profile name or both aws_access_key_id and aws_secret_access_key"
+                    )
+                elif not (
+                    len(v.aws_access_key_id) == AWS_ACCES_KEY_LEN
+                    and len(v.aws_secret_access_key) == AWS_SECRET_ACCES_KEY_LEN
+                ):
+                    raise ValueError("The access key or secret access key is invalid!")
         return v
 
     class Config:

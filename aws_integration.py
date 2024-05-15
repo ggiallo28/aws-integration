583db985ceeb43fdafea6e1d5459938d10920cd4
Full Name <email@amazon.com>
2024-05-15 14:58:41 +0000

    update validator

--- aws_integration.py
+++ aws_integration.py
@@ -11,12 +11,18 @@ class AWSClientSettings(BaseModel):
     credentials_profile_name: str = Field(default="")
     endpoint_url: str = Field(default="")
 
-    @model_validator(mode="before")
-    def validate_region(cls, v):
+    @model_validator(mode="after")
+    def validate(cls, v):
         session = boto3.Session()
         available_regions = session.get_available_regions("ec2")
-        if v["region_name"] not in available_regions:
-            raise ValueError(f"{v} is not a valid AWS region")
+        if v.region_name not in available_regions:
+            raise ValueError(f"{v.region_name} is not a valid AWS region")
+
+        if not v.credentials_profile_name:
+            if not v.aws_access_key_id or not v.aws_secret_access_key:
+                raise ValueError(
+                    "Either provide a credentials profile name or both aws_access_key_id and aws_secret_access_key"
+                )
         return v
 
     class Config:
